202f93fdf67e1301c8c896e6e0e57b27b1353647
GiuseppeMeliDevelhope <115168393+GiuseppeMeliDevelhope@users.noreply.github.com>
2023-02-05 13:57:09 +0100

    Update ExerciseThree.js

--- Esercizio-3-JSX/ExerciseThree.js
+++ Esercizio-3-JSX/ExerciseThree.js
@@ -1,20 +1,5 @@
 import React from "react";
 
-export class ExerciseThree extends React.Component{
-   constructor(props){
-    super(props)
-    this.state= {
-        a:5,
-        b:7
-    }
-
-   }
-
-   render(){
-    return(
-        <div>
-            <h2>The sum is: {this.state.a + this.state.b}</h2>
-        </div>
-    )
-   }
+function sum(a,b){
+  return <h2>The sum is: {a+b}</h2>;
 }
